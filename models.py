b1cfb8fa9fceee823da228b43876c8080d94a5e9
Dmytro <dimkanividimka4@gmail.com>
2026-08-01 00:23:52 +0300

    Setting up, initializing the database, and creating models.

---
+++ models.py
@@ -0,0 +1,25 @@
+from _pydatetime import date
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database import Base
+
+
+class DBAuthor(Base):
+    __tablename__ = "author"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    bio: Mapped[str] = mapped_column(String(700), nullable=False)
+    books: Mapped[list["DBBook"]] = relationship(back_populates="book_type")
+
+
+class DBBook(Base):
+    __tablename__ = "book"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    summary: Mapped[str] = mapped_column(String(1000), nullable=False)
+    publication_date: Mapped[date] = mapped_column(nullable=False, index=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey("book_type.id"))
+    cheese_type: Mapped["DBAuthor"] = relationship(back_populates="book")
